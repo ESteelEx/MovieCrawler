6942c648e457badfb6481d39c9e9c3a86b6bf712
ESteelEx <mathias@sangu.de>
2015-11-09 10:12:12 +0100

    First steps. - 1st search algorithms

---
+++ MovieCrawler.py
@@ -0,0 +1,10 @@
+import wx, zipfile, shutil, os, platform, subprocess, \
+    sys, string, warnings, threading
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ search_algorithms/filterMovies.py
@@ -0,0 +1,8 @@
+import string
+
+class seperator:
+    def __init__(self):
+        pass
+
+    def linkfilter(self, content):
+        pass
